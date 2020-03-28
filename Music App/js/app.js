2f6904fd4bba0b74a0f814ef988b1ae47d36947b
Erika Baltazar <erikabaltazar@erikas-air.home>
2020-03-27 18:11:29 -0700

    added all files
    added all files

---
+++ Music App/js/app.js
@@ -0,0 +1,57 @@
+$(function(){
+	window.addEventListener('load', () => {
+		const sounds = document.querySelectorAll('.sound');
+		const pads = document.querySelectorAll(".pads div");
+		const visual = document.querySelector('.visual');
+		const colors = [
+		"#60d394",
+		"red",
+		"pink",
+		"yellow",
+		"purple",
+		"blue"
+		];
+
+
+
+		//let's get going with the sound here
+		pads.forEach((pad, index) =>{
+			pad.addEventListener('click', function(){
+				sounds[index].currentTime = 0;
+				sounds[index].play();
+
+				createBubbles(index);
+			})
+		})
+
+		//create a function that makes bubbles
+		const createBubbles = (index) => {
+			const bubble = document.createElement("div");
+			visual.appendChild(bubble);
+			bubble.style.backgroundColor = colors[index];
+			bubble.style.animation = 'jump 1s ease';
+			bubble.addEventListener('animationend', function(){
+				visual.removeChild(this);
+			})
+		}
+	})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+});
